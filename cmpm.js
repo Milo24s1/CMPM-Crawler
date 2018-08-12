984e94fa55ebd6ee36425bfadb1303ee39513184
milo24s1 <na5703hansitha@gmail.com>
2018-08-12 15:02:26 +0530

    search by date feature added

--- cmpm.js
+++ cmpm.js
@@ -3,6 +3,77 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const readline = require('readline');
 const selector = '.col-xs-12.col-sm-6.col-md-4 a[title="download"]';
+const SEARCH_BY_PAGE = 1;
+const SEARCH_BY_DATE = 2;
+
+const getFileName = function (start,end,flag) {
+    if(flag==1){
+        return `page_${start}_to_page_${end}_url.txt`
+    }
+    else if(flag==2){
+        return `${start}_to_${end}_url.txt`
+    }
+}
+const extractLinksByDate = function(startDate,endDate,page){
+    let getOptions = {
+        url: 'http://fmderana.lk/jokes',
+        headers: {'User-agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0'},
+        jar: true,
+        followAllRedirects: false,
+
+    };
+
+        if(page!=1){
+            getOptions.url = `http://fmderana.lk/jokes/page/${page}`;
+        }
+        console.info('Crawling Page '+page);
+        request.get(getOptions, (error, response, html) => {
+            let isProceeding = false;
+            const startDateObject = new Date(startDate);
+            const endDateObject = new Date(endDate);
+            const regex = /\d\d\d\d_\d\d_\d\d/g;
+
+            if (!error) {
+                const $ = cheerio.load(html);
+                let urlString = $(selector).map(function(i, el) {
+
+                    if(/\d\d\d\d_\d\d_\d\d/.test($(this).attr('href'))){
+                        const currentDateObject = new Date($(this).attr('href').match(regex)[0]
+                            .split("_").join("-"));
+
+                        if(i==19){
+                            //check whether last element is within the date range, if it's behind proceed,
+                            if(currentDateObject >= endDateObject ){
+                                isProceeding = true;
+                            }
+                        }
+
+                        if(startDateObject >= currentDateObject && currentDateObject >= endDateObject){
+                            return $(this).attr('href');
+                        }
+
+                    }
+                    return '';
+
+                }).get().join('\n').trim();
+
+                fs.appendFile(getFileName(startDate,endDate,SEARCH_BY_DATE), urlString+'\n', 'utf8', () => {
+                    console.log('Data Saved');
+
+                });
+
+                if(isProceeding){
+                    extractLinksByDate(startDate,endDate,Number(page)+1);
+                }
+
+            }
+            else {
+                console.log(`Fail to Crawl`);
+                console.log(error);
+
+            }
+        });
+};
 
 const extractLinksByPageNumber = function(startPage,endPage){
     let getOptions = {
@@ -12,9 +83,11 @@ const extractLinksByPageNumber = function(startPage,endPage){
         followAllRedirects: false,
 
     };
-    while (startPage<=endPage){
+    let currentPage = startPage;
 
-        if(startPage!=1){
+    while (currentPage<=endPage){
+
+        if(currentPage!=1){
             getOptions.url = `http://fmderana.lk/jokes/page/${startPage}`;
         }
         request.get(getOptions, (error, response, html) => {
@@ -25,7 +98,7 @@ const extractLinksByPageNumber = function(startPage,endPage){
                     return $(this).attr('href');
                 }).get().join('\n');
 
-                fs.appendFile('url.txt', urlString, 'utf8', () => {
+                fs.appendFile(getFileName(startPage,endPage,SEARCH_BY_PAGE), urlString+'\n', 'utf8', () => {
                     console.log('Data Saved');
 
                 });
@@ -37,8 +110,8 @@ const extractLinksByPageNumber = function(startPage,endPage){
 
             }
         });
-        console.log(`Crawling Page ${startPage}`);
-        startPage++;
+        console.log(`Crawling Page ${currentPage}`);
+        currentPage++;
     }
 };
 
@@ -64,8 +137,19 @@ const readUserInput = function () {
                 }
             })
         }else if(answer==2){
-            console.log('Sorry \nThis method is not supported yet');
-            rl.close();
+            console.log('Extracting Links By Date (Descending order)');
+            rl.question('Starting Date ?\n',(start)=>{
+                if(/\d\d\d\d-\d\d-\d\d/.test(start)){
+                    rl.question('Ending Date ?\n',(end)=>{
+                        extractLinksByDate(start,end,1);
+                        rl.close();
+                    });
+                }
+                else {
+                    console.log('Please Enter valid date format (YYYY-MM-DD)');
+                    rl.close();
+                }
+            })
         }else {
             console.log('Please select a valid option');
             rl.close();
